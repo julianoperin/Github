3b5678f8cbdbc8caebb9031f6cf28d4aa65482ff
‘Juliano <julianoperins@gmail.com>
2020-05-24 00:09:27 -0400

    Added the alert message

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
+import Alert from "./components/layout/Alert";
+
 import axios from "axios";
 import "./App.css";
 
@@ -9,6 +11,7 @@ class App extends Component {
   state = {
     users: [],
     loading: false,
+    alert: null,
   };
 
   async componentDidMount() {
@@ -37,15 +40,24 @@ class App extends Component {
     this.setState({ users: [], loading: false });
   };
 
+  //! Set Alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg: msg, type: type } });
+
+    setTimeout(() => this.setState({ alert: null }), 3000);
+  };
+
   render() {
     return (
       <div>
         <Navbar title="GitHub Finder" icon="fab fa-github" />
         <div className="container">
+          <Alert alert={this.state.alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
             showClear={this.state.users.length > 0 ? true : false}
+            setAlert={this.setAlert}
           />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
